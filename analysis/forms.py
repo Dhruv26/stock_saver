bb4197539d2969625badcf8ba8536352a9e843dd
Dhruv Segat <dhruv.segat@matillion.com>
2020-01-01 16:26:02 +0530

    Fix add entry page

--- analysis/forms.py
+++ analysis/forms.py
@@ -1,16 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
-from wtforms import FieldList, FormField
+from wtforms import StringField, SelectField, SubmitField, FieldList, FormField
+from wtforms import validators
 
 
 class ParameterForm(FlaskForm):
-    type = SelectField(label='Parameter Type', choices=[('cross', 'CROSS'), ('val', 'VALUE')])
-    name = SelectField(label='Indicator', choices=[('rsi', 'RSI'), ('sma', 'SMA'), ('ema', 'EMA')])
-    value = StringField(label='Value')
+    type = SelectField(label='Parameter Type',
+                       choices=[('cross', 'CROSS'), ('val', 'VALUE')],
+                       validators=[validators.DataRequired()])
+    name = SelectField(label='Indicator',
+                       choices=[('rsi', 'RSI'), ('sma', 'SMA'), ('ema', 'EMA')],
+                       validators=[validators.DataRequired()])
+    value = StringField(label='Value', validators=[validators.DataRequired()])
 
 
 class AddEntryForm(FlaskForm):
-    stock_name = StringField('Stock Name')
-    flist = FieldList(FormField(ParameterForm), min_entries=1)
-    add_parameter = SubmitField('Add Parameter')
+    stock_name = StringField('Stock Name', [validators.DataRequired()])
+    indicators = FieldList(FormField(ParameterForm), min_entries=1)
     submit = SubmitField('Submit')

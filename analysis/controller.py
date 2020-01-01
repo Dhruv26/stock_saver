f09c8b68ef97f9e49b608381420725bb00e7287e
Vaibhav <vaibhavsegat18@gmail.com>
2020-01-01 10:48:13 +0000

    Adding get all

--- analysis/controller.py
+++ analysis/controller.py
@@ -1,4 +1,3 @@
-import json
 from flask import render_template, request
 from . import app
 from .forms import AddEntryForm
@@ -18,6 +17,10 @@ def add():
 
 @app.route('/addEntry', methods=['POST'])
 def add_entry():
-    indicators = dict()
     ob = AddEntryForm()
+    data = {
+        "stock_name": ob.data["stock_name"],
+        "indicators": [{k: v for k, v in d.items() if k != 'csrf_token'} for d in ob.data['indicators']]
+    }
+    MongoDb().insert(data)
     return 'Preparing to save...'
--- analysis/model.py
+++ analysis/model.py
@@ -4,5 +4,8 @@ from . import mongo
 class MongoDb:
     _table_name = 'trademon'
 
+    def get_all(self):
+        return list(mongo.db[self._table_name].find())
+
     def insert(self, data):
         mongo.db[self._table_name].insert(data)

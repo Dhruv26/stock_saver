81d2d28b4b5e2d16085f7db20dd4cb41a9dc5f58
Dhruv Segat <dhruv.segat@matillion.com>
2020-01-03 17:54:33 +0530

    Add CORS for future API usage

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,11 +1,13 @@
 from config import Config
 from flask import Flask
+from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app)
 mongo = PyMongo(app)
 bootstrap = Bootstrap(app)
 
